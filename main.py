950fe0f5749715112e5c5a2756b429d9fd562c35
Chikara Ohishi <pbigstone@gmail.com>
2021-02-24 20:53:51 +0900

    complete implementation

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 # Text to Morse Code Converter
 
+LOGO = """
+######## #######  ##   ##  ########          ########  #####            ##   ##   #####   ######    #####   #######            #####    #####   ######   #######
+#  ##  #  ##  ##   ## ##   #  ##  #          #  ##  # ##   ##           ### ###  ##   ##   ##  ##  ##   ##   ##  ##           ##   ##  ##   ##   ##  ##   ##  ##
+   ##     ##        ###       ##                ##    ##   ##           #######  ##   ##   ##  ##  ##        ##               ##       ##   ##   ##  ##   ##
+   ##     ####       #        ##                ##    ##   ##           ## # ##  ##   ##   #####    #####    ####             ##       ##   ##   ##  ##   ####
+   ##     ##        ###       ##                ##    ##   ##           ##   ##  ##   ##   ####         ##   ##               ##       ##   ##   ##  ##   ##
+   ##     ##  ##   ## ##      ##                ##    ##   ##           ##   ##  ##   ##   ## ##   ##   ##   ##  ##           ##   ##  ##   ##   ##  ##   ##  ##
+  ####   #######  ##   ##    ####              ####    #####            ##   ##   #####   ###  ##   #####   #######            #####    #####   ######   #######
+"""
+
 MORSE_CODE = {
     'A': '.-',
     'B': '-...',
@@ -39,3 +49,24 @@ MORSE_CODE = {
     '0': '-----',
 }
 
+
+def converter(input_text):
+    coded_string = ""
+    for char in list(input_text):
+        code = MORSE_CODE.get(char.upper())
+        # print(f"{char}: {code}")
+
+        # leave space as it is
+        if char == " ":
+            code = ""
+
+        if code != None:
+            coded_string += code + " "
+
+    return coded_string
+
+
+print(LOGO)
+text = input("Please input text(A-Z, 0-9)\n")
+coded_text = converter(text)
+print(f"Morse Code: {coded_text}")
